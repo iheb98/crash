e99f8dc9c15ac77b8f0da611112ae4442d7acecd
Ahmed <bt.ahmed.prod@gmail.com>
2024-03-14 03:33:48 +0100

    added flask server

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
+
+db_users = {
+    "John" : "Miami",
+    "David" : "Miami",
+    "Jane" : "London",
+    "Gabriella" : "Paris",
+    "Tanaka" : "Tokyo"
+}
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/search', methods=['GET'])
+def search():
+    result = ''
+    args = request.args
+    name = args.get('name')
+    location = args.get('location')
+
+    # result = db_users
+    if None not in (name, location):
+        result = {key: value for key, value in db_users.items() if key == name and value == location}
+    elif name is not None:
+        result = {key: value for key, value in db_users.items() if key == name}
+    elif location is not None:
+        result = {key: value for key, value in db_users.items() if value == location}
+
+    return result
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
